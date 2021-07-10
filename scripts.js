8e9f14b1ed3a1c7c1423b87d07b5e4534edfdd46
ZacchNaa <2016.naaz@gmail.com>
2021-07-10 10:42:12 +0000

    Improved the fib function with memoization

--- scripts.js
+++ scripts.js
@@ -1,7 +1,40 @@
-// function to show the n-th number of the fibonacci sequence
-const fib = (number) => {
-  if (number < 2) return 1;
-  return fib(number - 1) + fib(number - 2);
+/**-------------------------------
+ * fibonacci sequence
+ * -------------------------------
+ */
+// slow function
+// const fib = (number) => {
+//   if (number < 2) return 1;
+//   return fib(number - 1) + fib(number - 2);
+// };
+
+// fast function - better approach = memoization
+// the argument that will be passed at the function call
+// will be the key of the memo object and the value will be
+// the return value of the fib function
+const fib = (number, memo = {}) => {
+  // setup base cases
+  if (number in memo) return memo[number];
+  if (number <= 2) return 1;
+  // store fib sequence value in memo
+  memo[number] = fib(number - 1, memo) + fib(number - 2, memo);
+  // return the fib sequence
+  return memo[number];
 };
 
-console.log(fib(1));
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(50));
+
+//-------- checking for key evistence in objects
+// const obj = {
+//   fname: "Zacc",
+//   age: 29,
+//   color: "dark",
+// };
+
+// if ("fname" in obj) {
+//   console.log(obj);
+// }
+//-------- checking for key evistence in objects
